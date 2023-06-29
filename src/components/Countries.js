74ff5b84b62b646d89618a800c3102c25b73ab2e
mosidrum <mosiokanga@gmail.com>
2023-06-29 17:26:58 +0100

    Continue after house search

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { allCountries, filteredCountries } from '../redux/categorySlice';
-import Back from './Back';
+import '../css/countries.css';
 
 const Countries = () => {
   const [selectedCountry, setSelectedCountry] = useState(null);
@@ -19,68 +19,124 @@ const Countries = () => {
 
   if (!result) {
     return (
-      <>
-        <div>
-          <Back />
+      <div className="countries-container">
+        <div className="countries">
           {saved.map((each) => (
             <div
               key={each.alpha3Code}
               role="button"
+              className="country"
               tabIndex={0}
               onClick={() => handleDetails(each.alpha3Code)}
               onKeyDown={() => handleDetails(each.alpha3Code)}
             >
-              <li>{each.name}</li>
-              <li>{each.population}</li>
+              <img src={each.flag} alt={each.name} />
+              <h3>
+                {each.name}
+              </h3>
+              <div
+                className="bottom"
+              >
+                <li>
+                  Demonym:
+                  <span
+                    className="value"
+                  >
+                    {each.demonym}
+                  </span>
+                </li>
+                <li>
+                  Population:
+                  <span
+                    className="value"
+                  >
+                    {each.population}
+                  </span>
+                </li>
+                <button
+                  type="submit"
+                >
+                  View more
+                </button>
+              </div>
             </div>
           ))}
-          <div className="details">
-            {selectedCountry && (
-              <h1>
-                {Object.values(selectedCountry).map((value) => (
-                  <div
-                    key={value.alpha3Code}
-                  >
-                    <h2>{value.name}</h2>
-                    <li>{value.population}</li>
-                    <li>{value.subregion}</li>
-                  </div>
-                ))}
-              </h1>
-            )}
-          </div>
         </div>
-      </>
+        <div className="details">
+          {selectedCountry && (
+            <h1>
+              {Object.values(selectedCountry).map((value) => (
+                <div
+                  key={value.alpha3Code}
+                >
+                  <h2>{value.name}</h2>
+                  <li>{value.population}</li>
+                  <li>{value.subregion}</li>
+                </div>
+              ))}
+            </h1>
+          )}
+        </div>
+      </div>
     );
   }
   return (
-    <div>
-      <Back />
-      {result.map((each) => (
-        <div
-          key={each.alpha3Code}
-          role="button"
-          tabIndex={0}
-          onClick={() => handleDetails(each.alpha3Code)}
-          onKeyDown={() => handleDetails(each.alpha3Code)}
-        >
-          <li>{each.name}</li>
-          <li>{each.population}</li>
-        </div>
-      ))}
+    <div className="countries-container">
+      <div className="countries">
+        {saved.map((each) => (
+          <div
+            key={each.alpha3Code}
+            role="button"
+            className="country"
+            tabIndex={0}
+            onClick={() => handleDetails(each.alpha3Code)}
+            onKeyDown={() => handleDetails(each.alpha3Code)}
+          >
+            <img src={each.flag} alt={each.name} />
+            <h3>
+              {each.name}
+            </h3>
+            <div
+              className="bottom"
+            >
+              <li>
+                Demonym:
+                <span
+                  className="value"
+                >
+                  {each.demonym}
+                </span>
+              </li>
+              <li>
+                Population:
+                <span
+                  className="value"
+                >
+                  {each.population}
+                </span>
+              </li>
+              <button
+                type="submit"
+              >
+                View more
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
       <div className="details">
         {selectedCountry && (
-        <h1>
-          {Object.values(selectedCountry).map((value) => (
-            <div
-              key={value.alpha3Code}
-            >
-              <h2>{value.name}</h2>
-              <li>{value.population}</li>
-              <li>{value.subregion}</li>
-            </div>
-          ))}
-        </h1>
+          <h1>
+            {Object.values(selectedCountry).map((value) => (
+              <div
+                key={value.alpha3Code}
+              >
+                <h2>{value.name}</h2>
+                <li>{value.population}</li>
+                <li>{value.subregion}</li>
+              </div>
+            ))}
+          </h1>
         )}
       </div>
     </div>
